3ff5fbc0a2b6f925d03912fb1ac59a452260d424
Diyorbek <dedamirzayevdiyorbek9@gmail.com>
2025-10-03 21:55:06 +0500

    Initial commit: Create interactive hello world program

---
+++ dossier.py
@@ -0,0 +1,25 @@
+
+user_codename = input("Please enter the codename: ")
+user_real_name = input("Please enter your real name: ")
+user_gadget = input("What is the assigned gadget? ")
+user_operation_number = input("please enter operation number? ")
+user_mission_location = input("What is the mission location? ")
+
+
+
+print("****************************************")
+print("     TOP SECRET - AGENT DOSSIER         ")
+print("****************************************\n")
+
+
+print("Codename:    " + user_codename )
+print("Real Name:    " + user_real_name )
+print("Gadget:    " + user_gadget)
+
+print("----------------------------------------")
+print("\nMISSION DETAILS:   ")
+print("Operation ID:    " + user_operation_number)
+print("Location:    " + user_mission_location )
+print("---------------------------------------- \n")
+print("       ***For your eyes only***         \n")
+print("****************************************\n")
